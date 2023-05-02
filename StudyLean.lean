b3c78058819532157d2461a6038098450bb3872a
HiromiShikata <shikata.hiromi@gmail.com>
2023-05-02 00:44:29 +0000

    feat: use Real number

--- StudyLean.lean
+++ StudyLean.lean
@@ -1,1 +1,18 @@
-def hello := "world"
+import Mathlib.Data.Real.Basic
+import Mathlib.Data.List.Basic
+--import Mathlib.Analysis.SpecialFunctions.ExpLog.Basic.lean
+--import Mathlib.analysis.special_functions.exp_log
+--.Analysis.Special_functions.Exp_log
+--import Mathlib.lean3lib.init.default
+--import Mathlib.Data.Real.CauSeq
+
+
+def sum (a: List ℝ ) : ℝ := a.foldl (· + ·) 0
+
+#check [1, 2, 3, 4, 5]
+
+#eval sum [1, 2, 3, 4, 5]
+
+-- #eval real.exp 1
+--def exp (x: ℝ ) : ℝ := sum (List.range 100).map (λ n, x^n / n!)
+
--- lakefile.lean
+++ lakefile.lean
@@ -14,5 +14,6 @@ lean_exe «study-lean» {
   root := `Main
 }
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4.git"
+--require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "d14d735b5919dd1f87536d0a3347c039e632a408"
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "master"
   
